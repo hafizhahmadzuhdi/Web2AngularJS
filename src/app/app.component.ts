43ffe18d559bf50eede7a570ddf3196b71944695
Hafizh Ahmad Zuhdi,Hafizh H <h.zuhdi@student.fontys.nl>
2018-08-29 14:10:22 +0200

    Trying to populate a grid via an array of strings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>{{title}}</h1>
+  <h2>Heroes : {{myHero}} </h2>
+  <ul>
+  <li *ngFor="let hero of heroes">
+  {{ hero }}
+</li>
+</ul>
+ 
+  `
 })
 export class AppComponent {
-  title = 'test-angular';
+  title = 'Tour of Heroes';
+  heroes = ['Black Panther', 'Spiderman']
+  myHero = this.heroes[0];
 }
