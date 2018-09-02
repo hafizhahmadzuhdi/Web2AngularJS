ed0fd4238e8cc9eada8ad4a9c5f8228e3cf100c1
Do, Nhi E <nhiestherdo@gmail.com>
2018-09-02 15:55:44 +0200

    created departments components and populated grid via string array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <h1>{{title}}</h1>
-  <h2>Heroes : {{myHero}} </h2>
-  <ul>
-  <li *ngFor="let hero of heroes">
-  {{ hero }}
-</li>
-</ul>
- 
-  `
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
   heroes = ['Black Panther', 'Spiderman']
---
+++ src/app/departments/departments.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-departments',
+  templateUrl: './departments.component.html',
+  styleUrls: ['./departments.component.css']
+})
+export class DepartmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  departments = ['Accounting', 'Sales', 'Customer Serive', 'HR', 'Research and Development'];
+
+}
