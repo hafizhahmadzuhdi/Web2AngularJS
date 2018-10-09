495ad1fc50cf9117809485af814585cba67d4453
thentges <thibault.hentges@gmail.com>
2018-10-09 19:29:59 +0200

    clean start

--- src/app/tasks/taskdirective.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TaskdirectiveDirective } from './taskdirective.directive';
-
-describe('TaskdirectiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TaskdirectiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
